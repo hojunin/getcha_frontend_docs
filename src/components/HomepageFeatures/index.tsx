aca175a921c8f7e7612fc45d262818275c5c2716
Nick <dlsghwns@naver.com>
2022-07-24 23:57:29 +0900

    사진을 추가하면 어떨까

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -5,42 +5,50 @@ import Link from "@docusaurus/Link";
 interface BoxProps {
   title: string;
   description: string;
+  image: string;
 }
 
 const contents: BoxProps[] = [
   {
     title: "하하하 😆",
     description: "우리친해요 우리친해요 우리친해요 우리친해요 우리친해요",
+    image: "http://img.segye.com/content/image/2021/06/18/20210618504877.jpg",
   },
   {
     title: "히히히 😎",
     description:
       "우리 실력 좋아요 우리 실력 좋아요 우리 실력 좋아요 우리 실력 좋아요 ",
+    image: "http://img.segye.com/content/image/2021/06/18/20210618504877.jpg",
   },
   {
     title: "채신기술 사용함🚀",
     description:
       "채신기술만 취급합니다~ 채신기술만 취급합니다~ 채신기술만 취급합니다~",
+    image: "http://img.segye.com/content/image/2021/06/18/20210618504877.jpg",
   },
   {
-    title: "타이틀 1",
-    description: "설명 1,설명 1,설명 1,설명 1,설명 1,설명 1,설명 1,설명 1,",
+    title: "코어 서비스 팀",
+    description: "코어팀 프론트엔드 개발자가 하는 일",
+    image: "http://img.segye.com/content/image/2021/06/18/20210618504877.jpg",
   },
   {
-    title: "타이틀 1",
-    description: "설명 1,설명 1,설명 1,설명 1,설명 1,설명 1,설명 1,설명 1,",
+    title: "그로스 스쿼드",
+    description: "MAU 높이기 위한 스쿼드",
+    image: "http://img.segye.com/content/image/2021/06/18/20210618504877.jpg",
   },
   {
-    title: "타이틀 1",
-    description: "설명 1,설명 1,설명 1,설명 1,설명 1,설명 1,설명 1,설명 1,",
+    title: "머머머",
+    description: "또 뭐가 있겠찌",
+    image: "http://img.segye.com/content/image/2021/06/18/20210618504877.jpg",
   },
 ];
 
-const Box = ({ title, description }: BoxProps) => {
+const Box = ({ title, description, image }: BoxProps) => {
   return (
     <div className={styles.box}>
       <p className={styles.boxTitle}>{title}</p>
       <p className={styles.boxDescription}>{description}</p>
+      <img className={styles.image} src={image} />
     </div>
   );
 };
@@ -73,6 +81,7 @@ export default function HomepageFeatures(): JSX.Element {
               key={content.title}
               title={content.title}
               description={content.description}
+              image={content.image}
             />
           ))}
         </div>
